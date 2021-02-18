1ed7cbaa77853653c676ad2d2663061227924769
radharukminiY <rukku558@gmail.com>
2021-02-18 22:22:54 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import SearchBar from "./components/SearchBar";
+import SongsList from "./components/SongsList";
+import Player from "./components/Player";
+import Div100vh from "react-div-100vh";
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  *, ::before, ::after{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
+const AppWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div100vh>
+      <AppWrapper>
+        <GlobalStyle />
+        <SearchBar />
+        <SongsList />
+        <Player />
+      </AppWrapper>
+    </Div100vh>
   );
-}
+};
 
 export default App;
