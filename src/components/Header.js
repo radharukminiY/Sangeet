ae167f868f522ffb46f2d44915a1235cc9440915
radharukminiY <rukku558@gmail.com>
2021-02-19 18:32:28 +0530

    commit 4

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React, {useContext, useState} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import {SearchContext} from '../contexts/SearchContext';
+import './Header.css';
+
+const Header = ({history, location}) => {
+  const [searchText, setSearchText] = useState('');
+
+  const {startSearch} = useContext(SearchContext);
+
+  const setText = (e) => setSearchText(e.target.value);
+
+  const isHomePage = () => location.pathname === '/';
+
+  const handleKeyDown = (e) => {
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+      searchText.trim() !== '' && runSearch();
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchText.trim() !== '' && runSearch();
+  };
+
+  const runSearch = () => {
+    startSearch(searchText);
+    history.push('/');
+  };
+
+  return (
+    <div id="header">
+      <Link to="/" id="brand">
+        Sangeetam
+      </Link>
+      {!isHomePage() && (
+        // Wrapped with form for mobile support
+        <form id="header-search-form" onSubmit={handleSubmit}>
+          <input
+            id="header-search-bar"
+            type="text"
+            onChange={setText}
+            onKeyDown={handleKeyDown}
+            value={searchText}
+            placeholder="Search for music..."
+          />
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default withRouter(Header);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './main.css';
 
-import rootReducer from "./redux/reducers/rootReducer";
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
